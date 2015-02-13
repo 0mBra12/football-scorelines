7041fc06d99b2d4039604bf6931283e806bbeef4
Pierre Walch <pwalch@gmail.com>
2015-02-13 11:55:21 +0100

    Create european football research file.

---
+++ code/research/european_football.py
@@ -0,0 +1,14 @@
+# Stats about european football
+
+from api.match_utils import MatchUtils
+
+englishFolderList = ["../../dataset/actual/England/PL-2013-2014",
+                      "../../dataset/actual/England/PL-2012-2013",
+                      "../../dataset/actual/England/PL-2011-2012",
+                      "../../dataset/actual/England/PL-2010-2011",
+                      "../../dataset/actual/England/PL-2009-2010",
+                      "../../dataset/actual/England/PL-2008-2009",
+                      "../../dataset/actual/England/PL-2007-2008"]
+
+englishMatchList = matchList = MatchUtils.findMatchListInFolders(englishFolderList)
+
